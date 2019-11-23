43a9b726a83a5d6c990b1d168e1ca98ea76de045
Marc Nuñez <marc.nunezu@e-campus.uab.cat>
2019-11-23 19:47:56 +0100

    ADD Shirt detector

---
+++ src/blue_shirts_detector.py
@@ -0,0 +1,85 @@
+import os
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+
+from src import memory
+import cv2
+import numpy as np
+from tqdm import tqdm
+
+def get_blue_histograms(path: str, mask):
+    histograms = []
+    for file in os.listdir(path):
+        img = cv2.imread(os.path.join(path, file))
+        hist = cv2.calcHist([cv2.cvtColor(img, cv2.COLOR_BGR2HSV)], [0], mask, [180], [0, 180])  # HUE
+        # hist = cv2.calcHist([img], [0], mask, [180], [0, 180]) #BLUE
+        # hist = cv2.calcHist([cv2.cvtColor(img,cv2.COLOR_BGR2HSV)], [0, 1], mask, [180, 256], [0, 180, 0, 256]) #HUE AND SATURATION
+
+        histograms.append(hist)
+
+    return histograms
+
+@memory.cache()
+def compare_histograms(path: str, blue_hist_list, mask, threshold=2100):
+    histograms = []
+    images = []
+    for file in os.listdir(path):
+        blue = False
+        img = cv2.imread(os.path.join(path , file))
+        hist = cv2.calcHist([cv2.cvtColor(img,cv2.COLOR_BGR2HSV)], [0], mask, [180], [0, 180]) #HUE
+        for blue_hist in blue_hist_list:
+          shirt_flag = cv2.compareHist(blue_hist, hist, cv2.HISTCMP_INTERSECT)
+          if shirt_flag > threshold:
+            blue = True
+        if blue:
+          histograms.append(1)
+          images.append(file)
+        else:
+          histograms.append(0)
+    return np.array(histograms), images
+
+
+def ploting_blue_shirts(path: str, list_blue_shirts):
+
+    fig = plt.figure(figsize=(15, 15))
+
+    columns = 10
+    rows = 10
+    counter = 0
+
+    for path_img in tqdm(list_blue_shirts):
+        counter += 1
+        img = cv2.imread(os.path.join(path, path_img))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        ax = fig.add_subplot(rows, columns, counter)
+        #ax.title.set_text(path_img)
+        plt.imshow(img)
+        plt.axis('off')
+        if counter == (columns*rows):
+            counter = 0
+            plt.show()
+            plt.close()
+            fig = plt.figure(figsize=(15, 15))
+
+
+def main():
+    im_path_train = '../dataset/shirts/train'
+
+    im_path_blue = '../dataset/shirts/blue'
+
+    #Declaring a region of interest where most of the region of the shirt should be
+    mask = np.zeros((224, 224), np.uint8)
+    mask[75:150, 75:150] = 255
+
+    #Get list of blue shirts histograms
+    blue_hist_list = get_blue_histograms(im_path_blue, mask)
+
+    #Comparing each histogram of  train with each element of the blue shirt histogram list
+    labels, images = compare_histograms(im_path_train, blue_hist_list, mask)
+    print(np.unique(labels, return_counts=True))
+    print(images)
+
+    ploting_blue_shirts(im_path_train, images)
+
+if __name__ == '__main__':
+    main()
--- src/product_review.py
+++ src/product_review.py
@@ -1,6 +1,8 @@
 import string
 import pandas as pd
 from src import memory
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
 
 
 def read_csv(path: str)->pd.DataFrame:
@@ -10,16 +12,14 @@ def read_csv(path: str)->pd.DataFrame:
 
 @memory.cache()
 def clean_text(review_df: pd.DataFrame)->pd.DataFrame:
-    review_df['TEXT'] = review_df['TEXT'].apply(lambda  x: clean_punctuation(review_df['TEXT']))
-    #review_df['TEXT'] = review_df['TEXT'].apply(lambda  x: clean_digits(review_df['TEXT']))
+    review_df['TEXT'] = review_df['TEXT'].apply(lambda x: clean_punctuation(x.lower()))
+    review_df['TEXT'] = review_df['TEXT'].apply(lambda x: clean_digits(x))
     return review_df
 
 
 # Clean digits
 def clean_digits(review_text: str)->str:
     review_text = "".join([i for i in review_text if not i.isdigit()])
-    review_text = "".join([i for i in review_text if i not in string.punctuation])
-
     return review_text
 
 
@@ -29,13 +29,35 @@ def clean_punctuation(review_text: str)->str:
     return review_text
 
 
+@memory.cache()
+def tokenizer(review_df: pd.DataFrame)->pd.DataFrame:
+    tokenizer = RegexpTokenizer(r'\w+')
+    review_df['TEXT'] = review_df['TEXT'].apply(lambda x: tokenizer.tokenize(x))
+    return review_df
+
+
+def remove_stop_words_sentece(review_text: str)->str:
+    words = [w for w in review_text if w not in stopwords.words('english')]
+    return words
+
+
+@memory.cache()
+def remove_stop_words(review_df: pd.DataFrame)->pd.DataFrame:
+    review_df['TEXT'] = review_df['TEXT'].apply(lambda x: remove_stop_words_sentece(x))
+    return review_df
+
 
 def main():
     in_path = '../dataset/ratings.csv'
 
     review_df = read_csv(in_path)
+
     print(review_df.head())
-    review_df = clean_text(review_df.head())
+    review_df = clean_text(review_df.head(1000))
+    print(review_df.head())
+    review_df = tokenizer(review_df.head(1000))
+    print(review_df.head())
+    review_df = remove_stop_words(review_df.head(1000))
     print(review_df.head())
 
 if __name__ == '__main__':
